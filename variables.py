205765b61cd32a55ab3d778702ecd6eede2dca98
Katznicho <katznicho@gmail.com>
2022-05-08 07:51:33 +0300

    Created the varaibles file to handle any variable with in the program

---
+++ variables.py
@@ -0,0 +1,14 @@
+class Variables:
+    def __init__(self, scale_factor, min_neighbor):
+        self.scale_factor = scale_factor
+        self.min_neighbor = min_neighbor
+
+    def get_scale_factor(self):
+        return self.scale_factor
+
+    def get_min_neighbor(self):
+        return  self.min_neighbor
+
+
+
+
