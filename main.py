9a8070e52d53b86fe34601d33a259ab6da61f257
Katznicho <katznicho@gmail.com>
2022-05-08 07:51:58 +0300

    Updated the main file

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import cv2 as cv
-import numpy as np
+from variables import  Variables
+from images import Images
+MyVariables = Variables(1.1,2)
 
-girl_image = cv.imread("images/people.jpg")
-cv.imshow("Girl Image", girl_image)
+MyImage =  Images("images/people.jpg")
+girl = MyImage.read_image()
+MyImage.show_image("Girl Image", MyImage.read_image())
+MyImage.convert_to_gray()
+MyImage.show_image("Gray Image", MyImage.convert_to_gray())
 
-# convert image to gray scale
-gray = cv.cvtColor(girl_image, cv.COLOR_BGR2GRAY);
-cv.imshow("Gray Scale ", gray)
+
 # call the classifier
 haar = cv.CascadeClassifier('haar_face.xml')
-faces_rect = haar.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6)
+faces_rect = haar.detectMultiScale(MyImage.convert_to_gray(), scaleFactor=MyVariables.scale_factor,minNeighbors=MyVariables.get_min_neighbor())
 
-#print(f'The number of faces found are {faces_rect}')
+print(f'The number of faces found are {len(faces_rect)}')
 
 for (x, y, w, h) in faces_rect:
-    cv.rectangle(girl_image, (x, y), (x + w, y + h), (0, 255, 0), thickness=1)
-cv.imshow("Detected Faces", girl_image)
+    cv.rectangle(girl, (x, y), (x + w, y + h), (0, 255, 0), thickness=1)
+cv.imshow("Detected Faces", girl)
 
 cv.waitKey(0)
