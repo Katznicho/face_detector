24d274020337b5482f9e97be4bceee2e62930651
Katznicho <katznicho@gmail.com>
2022-05-08 07:50:41 +0300

    Created image file to handle image functionality

---
+++ images/images.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+
+class Images:
+
+    def __init__(self, path):
+        self.path = path
+
+    def read_image(self):
+        return cv.imread(self.path)
+
+    def convert_to_gray(self):
+        return cv.cvtColor(self.read_image(), cv.COLOR_BGR2GRAY)
+
+    def show_image(self, text , image):
+        return  cv.imshow(text, image)
+
+    def use_wait_key(self, delay):
+        return cv.waitKey(delay)
+
